c2902a984744aeec55ac7c87b4d9a059e1bc3e6c
Nha Le <hoanha020294@gmail.com>
2024-05-28 14:56:36 +0700

    add topic 22

---
+++ selenium/webdriver/Topic_22_Wait_01_Element_Status.java
@@ -0,0 +1,81 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class Topic_22_Wait_01_Element_Status {
+
+    WebDriver driver;
+    WebDriverWait explicitWait;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+    By reconfirmEmailTextbox = By.xpath("//input[@name='reg_email_confirmation__']");
+
+    @BeforeClass
+    public void beforeClass() {
+
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+        } else {
+            System.setProperty("webdriver.chrome.driver", projectPath + "/browserDrivers/chromedriver");
+        }
+
+        driver = new ChromeDriver();
+        explicitWait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+    }
+
+    @Test
+    public void TC_01_Visible() {
+        driver.get("https://www.facebook.com/");
+        driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).sendKeys("nha@gmail.com");
+
+        explicitWait.until(ExpectedConditions.visibilityOfElementLocated(reconfirmEmailTextbox));
+        Assert.assertTrue(driver.findElement(reconfirmEmailTextbox).isDisplayed());
+
+
+    }
+
+    @Test
+    public void TC_02_Invisible_In_DOM() {
+        driver.get("https://www.facebook.com/");
+        driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).sendKeys("nha@gmail.com");
+        driver.findElement(By.cssSelector("input[name='reg_email__")).clear();
+        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(reconfirmEmailTextbox));
+        Assert.assertFalse(driver.findElement(reconfirmEmailTextbox).isDisplayed());
+
+    }
+
+    @Test
+    public void TC_03_Invisible_Not_In_DOM() {
+        driver.findElement(By.xpath("//div[text()='Sign Up']/parent::div/preceding-sibling::img")).click();
+        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(reconfirmEmailTextbox));
+       // Assert.assertFalse(driver.findElement(reconfirmEmailTextbox).isDisplayed());
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+
+    public void sleepSeconds(long timeInsecond) {
+        try {
+            Thread.sleep(timeInsecond *1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
