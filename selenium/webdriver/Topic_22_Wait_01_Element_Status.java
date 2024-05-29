842578b41b2ebff5388070804f701571a7c68376
Nha Le <hoanha020294@gmail.com>
2024-05-29 17:13:33 +0700

    add tcs 4,5 for topic 22

--- selenium/webdriver/Topic_22_Wait_01_Element_Status.java
+++ selenium/webdriver/Topic_22_Wait_01_Element_Status.java
@@ -2,6 +2,7 @@ package webdriver;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
@@ -40,23 +41,27 @@ public class Topic_22_Wait_01_Element_Status {
     public void TC_01_Visible() {
         driver.get("https://www.facebook.com/");
         driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
+        sleepSeconds(3);
+
         driver.findElement(By.cssSelector("input[name='reg_email__']")).sendKeys("nha@gmail.com");
+        sleepSeconds(3);
 
         explicitWait.until(ExpectedConditions.visibilityOfElementLocated(reconfirmEmailTextbox));
         Assert.assertTrue(driver.findElement(reconfirmEmailTextbox).isDisplayed());
-
 
     }
 
     @Test
     public void TC_02_Invisible_In_DOM() {
         driver.get("https://www.facebook.com/");
+        // dk 2 element k xuat hien tren UI va van co trong html
         driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
+        sleepSeconds(3);
         driver.findElement(By.cssSelector("input[name='reg_email__']")).sendKeys("nha@gmail.com");
         driver.findElement(By.cssSelector("input[name='reg_email__")).clear();
-//        sleepSeconds(3);
-//        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(reconfirmEmailTextbox));
-//        Assert.assertFalse(driver.findElement(reconfirmEmailTextbox).isDisplayed());
+        sleepSeconds(3);
+        explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(reconfirmEmailTextbox));
+        Assert.assertFalse(driver.findElement(reconfirmEmailTextbox).isDisplayed());
 
     }
 
@@ -64,9 +69,37 @@ public class Topic_22_Wait_01_Element_Status {
     public void TC_03_Invisible_Not_In_DOM() {
         driver.get("https://www.facebook.com/");
         driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
+        sleepSeconds(3);
         driver.findElement(By.xpath("//div[text()='Sign Up']/parent::div/preceding-sibling::img")).click();
+        sleepSeconds(3);
         explicitWait.until(ExpectedConditions.invisibilityOfElementLocated(reconfirmEmailTextbox));
        // Assert.assertFalse(driver.findElement(reconfirmEmailTextbox).isDisplayed());
+    }
+    @Test
+    public void TC_04_Presence(){
+        driver.get("https://www.facebook.com/");
+        driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
+        sleepSeconds(2);
+        driver.findElement(By.cssSelector("input[name='reg_email__']")).sendKeys("nha@gmail.com");
+        sleepSeconds(2);
+        explicitWait.until(ExpectedConditions.presenceOfElementLocated(reconfirmEmailTextbox));
+        driver.findElement(By.cssSelector("input[name='reg_email__")).clear();
+        sleepSeconds(2);
+
+        explicitWait.until(ExpectedConditions.presenceOfElementLocated(reconfirmEmailTextbox));
+
+
+    }
+    @Test
+    public void TC_05_Staleness(){
+        driver.get("https://www.facebook.com/");
+        driver.findElement(By.xpath("//a[@data-testid='open-registration-form-button']")).click();
+        sleepSeconds(2);
+        WebElement reconfirmEmail = driver.findElement(reconfirmEmailTextbox);
+        driver.findElement(By.xpath("//div[text()='Sign Up']/parent::div/preceding-sibling::img")).click();
+        sleepSeconds(3);
+        explicitWait.until(ExpectedConditions.stalenessOf(reconfirmEmail));
+
     }
 
     @AfterClass
